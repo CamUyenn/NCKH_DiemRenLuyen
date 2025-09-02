9a80b7967434fcf26c7b4ebcd5df296a2d6aeffa
Pham Gia Khang <phamgiakhang290620042gmail.com>
2025-09-02 22:28:15 +0700

    Update connect database

--- Backend/initialize/connectdb.go
+++ Backend/initialize/connectdb.go
@@ -8,12 +8,12 @@ import (
 	"gorm.io/gorm"
 )
 
-var db *gorm.DB
+var DB *gorm.DB
 
 func ConnectDB() {
 	var err error
 	dsn := os.Getenv("DB_URL")
-	db, err = gorm.Open(sqlserver.Open(dsn), &gorm.Config{})
+	DB, err = gorm.Open(sqlserver.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		log.Fatal("Connect to database failed")
