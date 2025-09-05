33ac763e3b214706be6e19782f23c40df0e64b8a
Pham Gia Khang <phamgiakhang290620042gmail.com>
2025-09-05 11:56:27 +0700

    Add lopsinhhoathocky model

---
+++ Backend/model/lopsinhhoathocky.go
@@ -0,0 +1,12 @@
+package model
+
+type LopSinhHoatHocKy struct {
+	MaLopSinhHoatHocKy string `json:"ma_lop_sinh_hoat_hoc_ky" gorm:"primaryKey"`
+	MaHocKy            string `json:"ma_hoc_ky"`
+	MaLopSinhHoat      string `json:"ma_lop_sinh_hoat"`
+	MaLopTruong        string `json:"ma_lop_truong"`
+	MaGiangVien        string `json:"ma_giang_vien"`
+	MaTruongKhoa       string `json:"ma_truong_khoa"`
+	MaChuyenVienDaoTao string `json:"ma_chuyen_vien_dao_tao"`
+	TongDiem           int    `json:"tong_diem"`
+}
