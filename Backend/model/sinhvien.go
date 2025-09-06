709b4e5e876dd6d191ef75e6ca523058af7dad1b
Tuấn Kiệt <phamtuankiet202@gmail.com>
2025-09-06 23:47:58 +0700

    Update name Ma_GV to MaGiangVien and Ma_SV to MaSinhVien

--- Backend/model/sinhvien.go
+++ Backend/model/sinhvien.go
@@ -3,13 +3,13 @@ package model
 import "time"
 
 type SinhVien struct {
-	Ma_SV    string    `json:"ma_sinh_vien" gorm:"primaryKey"`
-	HoDem    string    `json:"ho_dem"`
-	Ten      string    `json:"ten"`
-	GioiTinh bool      `json:"gioi_tinh"`
-	NgaySinh time.Time `json:"ngay_sinh"`
-	NoiSinh  string    `json:"noi_sinh"`
-	MatKhau  string    `json:"mat_khau"`
+	MaSinhVien string    `json:"ma_sinh_vien" gorm:"primaryKey"`
+	HoDem      string    `json:"ho_dem"`
+	Ten        string    `json:"ten"`
+	GioiTinh   bool      `json:"gioi_tinh"`
+	NgaySinh   time.Time `json:"ngay_sinh"`
+	NoiSinh    string    `json:"noi_sinh"`
+	MatKhau    string    `json:"mat_khau"`
 }
 
 func (SinhVien) TableName() string {
