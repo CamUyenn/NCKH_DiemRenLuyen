779c5fefba3fd585aeb15a34fe11c338db9b4300
Pham Gia Khang <phamgiakhang290620042gmail.com>
2025-09-09 23:03:01 +0700

    Update model admin

--- Backend/model/admin.go
+++ Backend/model/admin.go
@@ -1,6 +1,10 @@
 package model
 
 type Admin struct {
-	MaAdmin     string `json:"ma_admin" gorm:"primaryKey"`
-	MaGiangVien string `json:"ma_giang_vien"`
+	MaAdmin              string `gorm:"size:256;primaryKey" json:"ma_admin"`
+	MaGiangVienThamChieu string `gorm:"size:256" json:"ma_giang_vien_tham_chieu"`
+}
+
+func (Admin) TableName() string {
+	return "Admin"
 }
