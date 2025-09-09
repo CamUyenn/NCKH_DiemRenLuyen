016ace5f7f27b8b2ed41f686415ec7cd74453a64
Pham Gia Khang <phamgiakhang290620042gmail.com>
2025-09-09 23:05:01 +0700

    Update model lopsinhhoathocky

--- Backend/model/lopsinhhoathocky.go
+++ Backend/model/lopsinhhoathocky.go
@@ -1,12 +1,19 @@
 package model
 
 type LopSinhHoatHocKy struct {
-	MaLopSinhHoatHocKy string `json:"ma_lop_sinh_hoat_hoc_ky" gorm:"primaryKey"`
-	MaHocKy            string `json:"ma_hoc_ky"`
-	MaLopSinhHoat      string `json:"ma_lop_sinh_hoat"`
-	MaLopTruong        string `json:"ma_lop_truong"`
-	MaGiangVien        string `json:"ma_giang_vien"`
-	MaTruongKhoa       string `json:"ma_truong_khoa"`
-	MaChuyenVienDaoTao string `json:"ma_chuyen_vien_dao_tao"`
-	TongDiem           int    `json:"tong_diem"`
+	MaLopSinhHoatHocKy     string `gorm:"size:256;primaryKey" json:"ma_lop_sinh_hoat_hoc_ky"`
+	MaHocKyThamChieu       string `gorm:"size:256" json:"ma_hoc_ky_tham_chieu"`
+	MaLopSinhHoatThamChieu string `gorm:"size:256" json:"ma_lop_sinh_hoat_tham_chieu"`
+	MaLopTruong            string `gorm:"size:256" json:"ma_lop_truong"`
+	MaGiangVienCoVan       string `gorm:"size:256" json:"ma_giang_vien_co_van"`
+	MaTruongKhoa           string `gorm:"size:256" json:"ma_truong_khoa"`
+	MaChuyenVienDaoTao     string `gorm:"size:256" json:"ma_chuyen_vien_dao_tao"`
+	TongDiem               int    `json:"tong_diem"`
+
+	Lop   LopSinhHoat `gorm:"foreignKey:MaLopSinhHoatThamChieu;references:MaLopSinhHoat"`
+	HocKy HocKy       `gorm:"foreignKey:MaHocKyThamChieu;references:MaHocKy"`
+}
+
+func (LopSinhHoatHocKy) TableName() string {
+	return "LopSinhHoatHocKy"
 }
