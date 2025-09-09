f802218be2bb637a9b1a37c3d3890a84f1b59d5b
Pham Gia Khang <phamgiakhang290620042gmail.com>
2025-09-09 23:03:53 +0700

    Update model bangdiemchitiet

--- Backend/model/bangdiemchitiet.go
+++ Backend/model/bangdiemchitiet.go
@@ -1,13 +1,20 @@
 package model
 
 type BangDiemChiTiet struct {
-	MaTieuChi    string `json:"ma_tieu_chi" gorm:"primaryKey"`
-	MaBangDiem   string `json:"ma_bang_diem"`
-	TenTieuChi   string `json:"ten_tieu_chi"`
-	Muc          int    `json:"muc"`
-	Diem         int    `json:"diem"`
-	MoTaDiem     string `json:"mo_ta_diem"`
-	MaTieuChiCha string `json:"ma_tieu_chi_cha"`
-	LoaiTieuChi  string `json:"loai_tieu_chi"`
-	SoLan        int    `json:"so_lan"`
+	MaTieuChi           string `gorm:"size:256;primaryKey" json:"ma_tieu_chi"`
+	MaBangDiemThamChieu string `gorm:"size:256" json:"ma_bang_diem_tham_chieu"`
+	TenTieuChi          string `json:"ten_tieu_chi"`
+	Muc                 int    `json:"muc"`
+	Diem                int    `json:"diem"`
+	MoTaDiem            string `json:"mo_ta_diem"`
+	MaTieuChiCha        string `gorm:"size:256" json:"ma_tieu_chi_cha"`
+	LoaiTieuChi         string `json:"loai_tieu_chi"`
+	SoLan               int    `json:"so_lan"`
+
+	BangDiem                    BangDiem                    `gorm:"foreignKey:MaBangDiemThamChieu;references:MaBangDiem"`
+	SinhVienDiemRenLuyenChiTiet SinhVienDiemRenLuyenChiTiet `gorm:"foreignKey:MaTieuChiThamChieu;references:MaTieuChi"`
+}
+
+func (BangDiemChiTiet) TableName() string {
+	return "BangDiemChiTiet"
 }
