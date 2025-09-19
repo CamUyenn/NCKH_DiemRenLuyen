89aed5cd445defeeb7e2b20027e2b9b807df12e0
Pham Gia Khang <phamgiakhang290620042gmail.com>
2025-09-19 18:05:52 +0700

    Update source router in main.go

--- Backend/main.go
+++ Backend/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"Backend/controller"
+	"Backend/controller/bangdiem"
+	"Backend/controller/hocky"
 	"Backend/controller/tieuchi"
 	"Backend/initialize"
 	"Backend/migrate"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -19,8 +22,22 @@ func main() {
 
 	router := gin.Default()
 
+	// Config CORS
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"}, // FE dev server
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
+
 	router.POST("/api/login", controller.Login)
-	router.POST("/api/tieuchi", tieuchi.TaoTieuChi)
+	router.POST("/api/taotieuchi", tieuchi.TaoTieuChi)
+	router.POST("/api/taobangdiem", bangdiem.TaoBangDiem)
+	router.DELETE("/api/xoatieuchi/:id", tieuchi.XoaTieuChi)
+	router.DELETE("/api/xoabangdiem/:id", bangdiem.XoaBangDiem)
+	router.DELETE("/api/xoahocky/:id", hocky.XoaHocKy)
+	router.GET("/api/xembangdiem", bangdiem.XemBangDiem)
 
 	router.Run()
 }
