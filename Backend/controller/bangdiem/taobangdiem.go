ce9d5d2f3ac53ec91a590eee53e9ecda9a379d9b
Pham Gia Khang <phamgiakhang290620042gmail.com>
2025-10-05 22:08:19 +0700

    Update api taobangdiem

--- Backend/controller/bangdiem/taobangdiem.go
+++ Backend/controller/bangdiem/taobangdiem.go
@@ -1,12 +1,9 @@
 package bangdiem
 
 import (
-	"Backend/controller/bangdiemcham"
-	"Backend/controller/hocky"
 	"Backend/initialize"
 	"Backend/model"
-	"strconv"
-	"time"
+	"Backend/service/hockyhethong"
 
 	"github.com/gin-gonic/gin"
 )
@@ -23,57 +20,24 @@ func TaoBangDiem(c *gin.Context) {
 		return
 	}
 
-	// Check if hocky already exists
-	var count int64
-	result := initialize.DB.Model(&model.HocKy{}).Where("ma_hoc_ky = ?", hockycheck.MaHocKy).Count(&count)
+	// Create HocKy
+	hockyhethong.TaoHocKyHeThong(c, hockycheck.MaHocKy)
+
+	// Create MaBangDiem
+	bangdiemcheck.MaBangDiem = hockycheck.MaHocKy + "_BD"
+	bangdiemcheck.MaHocKyThamChieu = hockycheck.MaHocKy
+
+	// Create BangDiem
+	result := initialize.DB.Create(&bangdiemcheck)
 	if result.Error != nil {
 		c.JSON(400, gin.H{
-			"error": "Count hocky in database failed",
+			"error": "Create bangdiem failed",
 		})
 		return
-	}
-
-	if count == 0 {
-		// Create HocKy
-		hocky.TaoHocKy(c, hockycheck.MaHocKy)
-	}
-	// Check if BangDiem already exists
-	result = initialize.DB.Model(&model.BangDiem{}).Where("ma_hoc_ky_tham_chieu = ?", hockycheck.MaHocKy).Count(&count)
-	if result.Error != nil {
-		c.JSON(400, gin.H{
-			"error": "Count bangdiem in database failed",
-		})
-		return
-	}
-
-	// Check the count of existing BangDiem in the HocKy
-	if count < 11 {
-		// Create BangDiem
-		bangdiemcheck.MaBangDiem = hockycheck.MaHocKy + "_BD" + strconv.FormatInt(count, 10)
-		bangdiemcheck.MaHocKyThamChieu = hockycheck.MaHocKy
-		bangdiemcheck.NgayTao = time.Now()
-		bangdiemcheck.ThoiHanNop = time.Now().AddDate(0, 1, 0)
-
-		result = initialize.DB.Create(&bangdiemcheck)
-		if result.Error != nil {
-			c.JSON(400, gin.H{
-				"error": "Create bangdiem failed",
-			})
-			return
-		} else {
-			c.JSON(200, gin.H{
-				"message": "Create bangdiem successfully",
-			})
-
-			// Create BangDiemCham
-			bangdiemcham.TaoBangDiemCham(c, bangdiemcheck.MaBangDiem)
-			return
-		}
 	} else {
-		c.JSON(400, gin.H{
-			"error": "Bangdiem limit create for this hocky",
+		c.JSON(200, gin.H{
+			"message": "Create bangdiem successfully",
 		})
 		return
 	}
-
 }
