c527df4f6288bd34346e9b182662b3607aee5198
Pham Gia Khang <phamgiakhang290620042gmail.com>
2025-11-25 20:25:58 +0700

    Update data output

--- Backend/controller/tieuchi/xemtieuchicham.go
+++ Backend/controller/tieuchi/xemtieuchicham.go
@@ -19,6 +19,11 @@ func XemTieuChiCham(c *gin.Context) {
 		MaTieuChiCha                  string `json:"ma_tieu_chi_cha"`
 		LoaiTieuChi                   string `json:"loai_tieu_chi"`
 		SoLan                         int    `json:"so_lan"`
+		DiemSinhVienDanhGia           int    `json:"diem_sinh_vien_danh_gia"`
+		DiemLopTruongDanhGia          int    `json:"diem_lop_truong_danh_gia"`
+		DiemGiangVienDanhGia          int    `json:"diem_giang_vien_danh_gia"`
+		DiemTruongKhoaDanhGia         int    `json:"diem_truong_khoa_danh_gia"`
+		DiemChuyenVienDaoTao          int    `json:"diem_chuyen_vien_dao_tao"`
 	}
 
 	// Get masinhvien and mahocky from URL
