f235b96960f51b35e22f1257edc6f2d191d34670
Pham Gia Khang <phamgiakhang290620042gmail.com>
2025-11-26 21:41:28 +0700

    Update copy diem in database

--- Backend/controller/tieuchi/saochepdiem.go
+++ Backend/controller/tieuchi/saochepdiem.go
@@ -24,6 +24,8 @@ func SaoChepDiem(c *gin.Context) {
 	// Check type and copy diem
 	switch datainput.Type {
 	case "loptruong":
+		// Copy diemsinhviendanhgia to diemloptruong in SinhVienDiemRenLuyenChiTiet
+
 		result := initialize.DB.Exec("UPDATE SinhVienDiemRenLuyenChiTiet SET diem_lop_truong_danh_gia = diem_sinh_vien_danh_gia WHERE ma_sinh_vien_diem_ren_luyen_tham_chieu = ?", datainput.MaBangDiem)
 		if result.Error != nil {
 			c.JSON(400, gin.H{
@@ -32,10 +34,21 @@ func SaoChepDiem(c *gin.Context) {
 			return
 		}
 
+		// Copy tongdiemsinhvien to tongdiemloptruong in SinhVienDiemRenLuyen
+
+		result = initialize.DB.Exec("UPDATE SinhVienDiemRenLuyen SET tong_diem_lop_truong = tong_diem_sinh_vien, xep_loai_lop_truong = xep_loai_sinh_vien WHERE ma_sinh_vien_diem_ren_luyen = ?", datainput.MaBangDiem)
+		if result.Error != nil {
+			c.JSON(400, gin.H{
+				"error": "Copy tongdiemsinhviendanhgia failed",
+			})
+			return
+		}
+
 		c.JSON(200, gin.H{
 			"message": "Copy diemsinhviendanhgia success",
 		})
 	case "giangvien":
+		// Copy diemloptruong to diemgiangvien in SinhVienDiemRenLuyenChiTiet
 		result := initialize.DB.Exec("UPDATE SinhVienDiemRenLuyenChiTiet SET diem_giang_vien_danh_gia = diem_lop_truong_danh_gia WHERE ma_sinh_vien_diem_ren_luyen_tham_chieu = ?", datainput.MaBangDiem)
 		if result.Error != nil {
 			c.JSON(400, gin.H{
@@ -44,10 +57,20 @@ func SaoChepDiem(c *gin.Context) {
 			return
 		}
 
+		// Copy tongdiemloptruong to tongdiemcovan in SinhVienDiemRenLuyen
+		result = initialize.DB.Exec("UPDATE SinhVienDiemRenLuyen SET tong_diem_co_van = tong_diem_lop_truong, xep_loai_co_van = xep_loai_lop_truong WHERE ma_sinh_vien_diem_ren_luyen = ?", datainput.MaBangDiem)
+		if result.Error != nil {
+			c.JSON(400, gin.H{
+				"error": "Copy tongdiemloptruong failed",
+			})
+			return
+		}
+
 		c.JSON(200, gin.H{
 			"message": "Copy diemgiangviendanhgia success",
 		})
 	case "truongkhoa":
+		// Copy diemgiangvien to diemtruongkhoa in SinhVienDiemRenLuyenChiTiet
 		result := initialize.DB.Exec("UPDATE SinhVienDiemRenLuyenChiTiet SET diem_truong_khoa_danh_gia = diem_giang_vien_danh_gia WHERE ma_sinh_vien_diem_ren_luyen_tham_chieu = ?", datainput.MaBangDiem)
 		if result.Error != nil {
 			c.JSON(400, gin.H{
@@ -56,14 +79,33 @@ func SaoChepDiem(c *gin.Context) {
 			return
 		}
 
+		// Copy tongdiemcovan to tongdiemtruongkhoa in SinhVienDiemRenLuyen
+		result = initialize.DB.Exec("UPDATE SinhVienDiemRenLuyen SET tong_diem_truong_khoa = tong_diem_co_van, xep_loai_truong_khoa = xep_loai_co_van WHERE ma_sinh_vien_diem_ren_luyen = ?", datainput.MaBangDiem)
+		if result.Error != nil {
+			c.JSON(400, gin.H{
+				"error": "Copy tongdiemcovan failed",
+			})
+			return
+		}
+
 		c.JSON(200, gin.H{
 			"message": "Copy diemtruongkhoadanhgia success",
 		})
 	case "chuyenvien":
+		// Copy diemtruongkhoa to diemchuyenviendaotao in SinhVienDiemRenLuyenChiTiet
 		result := initialize.DB.Exec("UPDATE SinhVienDiemRenLuyenChiTiet SET diem_chuyen_vien_dao_tao = diem_truong_khoa_danh_gia WHERE ma_sinh_vien_diem_ren_luyen_tham_chieu = ?", datainput.MaBangDiem)
 		if result.Error != nil {
 			c.JSON(400, gin.H{
 				"error": "Copy diemchuyenviendaotao failed",
+			})
+			return
+		}
+
+		// Copy tongdiemtruongkhoa to tongdiemchuyenviendaotao in SinhVienDiemRenLuyen
+		result = initialize.DB.Exec("UPDATE SinhVienDiemRenLuyen SET tong_diem_chuyen_vien_dao_tao = tong_diem_truong_khoa, xep_loai_chuyen_vien_dao_tao = xep_loai_truong_khoa WHERE ma_sinh_vien_diem_ren_luyen = ?", datainput.MaBangDiem)
+		if result.Error != nil {
+			c.JSON(400, gin.H{
+				"error": "Copy tongdiemtruongkhoa failed",
 			})
 			return
 		}
