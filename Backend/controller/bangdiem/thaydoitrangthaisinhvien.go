f2ad937ebdf60d9570b95b0e901ff5aae1409a69
Pham Gia Khang <phamgiakhang290620042gmail.com>
2025-12-05 20:25:18 +0700

    Update api thaydoitrangthai to thaydoitrangthaisinhvien for only sinhvien

---
+++ Backend/controller/bangdiem/thaydoitrangthaisinhvien.go
@@ -0,0 +1,44 @@
+package bangdiem
+
+import (
+	"Backend/initialize"
+	"Backend/model"
+
+	"github.com/gin-gonic/gin"
+)
+
+func ThayDoiTrangThaiSinhVien(c *gin.Context) {
+	type Datainput struct {
+		MaBangDiem []string `json:"mabangdiem"`
+		Type       string   `json:"type"`
+	}
+
+	var datainput Datainput
+	if err := c.ShouldBindJSON(&datainput); err != nil {
+		c.JSON(400, gin.H{
+			"error": "Fetch mabangdiem and type from JSON failed",
+		})
+		return
+	}
+
+	var trangthaioutput string
+
+	switch datainput.Type {
+	case "sinhvien":
+		trangthaioutput = "Sinh Viên Đã Chấm"
+	case "loptruong":
+		trangthaioutput = "Lớp Trưởng Đã Chấm"
+	}
+
+	result := initialize.DB.Model(&model.SinhVienDiemRenLuyen{}).Where("ma_sinh_vien_diem_ren_luyen IN ?", datainput.MaBangDiem).Update("trang_thai", trangthaioutput)
+	if result.Error != nil {
+		c.JSON(400, gin.H{
+			"error": "Failed to update status in the database",
+		})
+		return
+	}
+
+	c.JSON(200, gin.H{
+		"message": "Status updated successfully",
+	})
+}
